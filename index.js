0ee91dfd95980a61fd506b4d7a9f68f6eb7fe469
Vincent BRIDIER <vincent.bridier@ldc.fr>
2022-09-28 07:46:25 +0200

    clean

--- index.js
+++
@@ -1,5 +0,0 @@
-import { NativeModules } from 'react-native';
-
-const { BCryptBridge } = NativeModules;
-
-export default BCryptBridge;
--- ios/BCryptBridge.h
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface BCryptBridge : NSObject <RCTBridgeModule>
-
-@end
